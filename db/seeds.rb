f35554ce8d6a4365afbb9d81342dc88578d9feeb
Yamasaki Yutaka <yutaka@Yamasakis-MacBook-Pro.local>
2018-07-09 18:07:20 +0800

    create authentication

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+User.create(
+	name: "Kawasaki",
+	email: "uminchuvirtual@gmail.com",
+	password: "password",
+	admin: true
+	)
