c3a2a66bfed8d44584fab0c2d310a9853542a4d8
YutakaYamasaki <40820397+YutakaYamasaki@users.noreply.github.com>
2018-07-11 16:25:58 +0800

    create admin_user edit (#4)
    * create admin_user
    
    * Fixed admin_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,45 @@
 class UsersController < ApplicationController
-	def new
-		@user = User.new
-	end
+# before_action :correct_user, only: [:edit,:update]
+  def new
+    @user = User.new
+  end
 
-	def create
-		@user = User.new(user_params)
-		if @user.save 
-			flash[:success] = "Welcome to E-learning"
-			log_in @user
-			redirect_to @user
-		else
-			render "new"
-		end
-	end
+  def create
+    @user = User.new(user_params)
+    if @user.save 
+      flash[:success] = "Welcome to E-learning"
+      log_in @user
+      redirect_to @user
+    else
+      render "new"
+    end
+  end
 
-	def index
-		
-	end
+  def index
+    @users = User.paginate(page: params[:page],per_page:15)
+  end
 
-	def show
-		@user = User.find(params[:id])
-	end
-	
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+      if @user.update_attributes(user_params);
+        flash[:success] = "Updated uccessfully"
+        redirect_to @user
+    else
+        render 'edit'
+      end
+  end
+  
 private
-	def user_params
-		params.require(:user).permit(:name,:email,:password,:password_confirmation)
-	end
-	
+  def user_params
+    params.require(:user).permit(:name,:email,:password,:password_confirmation)
+  end
+  
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,5 +7,11 @@ module SessionsHelper
     session.delete(:user_id)
     flash[:info] = "You successfully logged out"
   end
-  
+ def current_user
+ 	@current_user ||=  User.find_by(id: session[:user_id])
+ end
+
+ def logged_in?
+ 	! current_user.nil?
+ end
 end
